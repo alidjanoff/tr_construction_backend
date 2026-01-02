cd25e7a91fdcc0f0bd94f4a9ffd2ece303cbc910
alidjanoff <mrdeathly007@gmail.com>
2026-01-03 01:39:08 +0400

    Fix

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log(`MongoDB Connection Status: Success`);
     } catch (error) {
-        console.error(`Error: ${error.message}`);
+        console.error(`MongoDB Connection Status: Error ${error.message}`);
         process.exit(1);
     }
 };
